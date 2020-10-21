23e5899ff12f2743ad78b232e9497dd87a3612a9
katherinex <kathexio@gmail.com>
2020-10-20 20:41:28 -0500

    Dispatch actions and suscribe to the store

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { renderTodos } from './utils';
+import * as fromStore from './store/index';
+import { RemoveTodo } from './store/index';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
+
+// console.log(store.value);
 
 button.addEventListener(
   'click',
@@ -12,16 +22,30 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch(new fromStore.AddTodo(payload));
+
+    console.log(store.value);
 
     input.value = '';
   },
   false
 );
 
+const unsubscribe = store.subscribe(state => {
+  console.log('const');
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener('click', unsubscribe, false);
+
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
+    const data = JSON.parse(target.getAttribute('data-todo') as any);
+    store.dispatch(new RemoveTodo(data));
+    console.log(data);
   }
 });
+
+// store.subscribe(state => console.log('STATE:::', state));
